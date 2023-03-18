989786a17d60bbeb8119c02945f899c45b5d6c2d
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2023-03-18 03:33:05 +0330

    Changed file manager create button icon

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,6 +1,6 @@
 import Hierarchy from "@components/Hierarchy";
 import { useHierarchy } from "@context/hierarchy";
-import { AddSquare } from "iconsax-react";
+import { Add } from "iconsax-react";
 
 export default function Sidebar() {
   const { hierarchy, setHierarchy, updateHierarchyChild, removeHierarchyChild } = useHierarchy();
@@ -13,8 +13,11 @@ export default function Sidebar() {
     <div className="flex w-full flex-col items-start">
       <div className="flex w-full items-center justify-between">
         <h2 className="mb-2 text-lg font-bold">File Manager</h2>
-        <button className=" flex items-center gap-1 rounded-lg bg-slate-200 py-1 px-2 text-sm" onClick={() => handleCreate()}>
-          <AddSquare size="18" /> Create
+        <button
+          className=" flex items-center gap-1 rounded-lg bg-slate-200 py-1 px-2 text-sm"
+          onClick={() => handleCreate()}
+        >
+          <Add size="18" className="rotate-90" /> Create
         </button>
       </div>
       {hierarchy.map((item, index) => (
